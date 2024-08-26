db7291ec3927c4a3d480e4d3c509fea4240d7a17
jingyima <jinma717@gmail.com>
2024-08-26 14:09:21 -0400

    Finished the game algo

--- javascript.js
+++ javascript.js
@@ -73,6 +73,19 @@ function GameController(
         console.log(`${getActivePlayer().name}'s turn.`);
     };
 
+    const checkWinner = () => {
+        const b = board.getBoard().map(row => row.map(cell => cell.getValue()));
+
+        for(i=0; i<3; i++) {
+            if(b[i][0] === b[i][1] && b[i][1] === b[i][2] && b[i][0] !=0) return players.find(p => p.token === b[i][0]);
+            if(b[0][i] === b[1][i] && b[1][i] === b[2][i] && b[0][i] !=0) return players.find(p => p.token === b[0][i]);
+        }
+
+        if(b[0][0] === b[1][1] && b[1][1] === b[2][2] && b[0][0] !=0) return players.find(p => p.token === b[0][0]);
+        if (b[0][2] === b[1][1] && b[1][1] === b[2][0] && b[0][2] != 0) return players.find(p => p.token === b[0][2]);
+        return 0;
+    };
+
     const playRound = (row, column) => {
         console.log(
             `Dropping ${getActivePlayer().name}'s token into row ${row} and column ${column}...`
@@ -81,23 +94,9 @@ function GameController(
 
         const result = {
             success: moveResult,
-            player: getActivePlayer()
+            player: getActivePlayer(),
         };
         
-        const checkWinner = () => {
-            const b = board.getBoard().map(row => row.map(cell => cell.getValue()));
-
-            for(i=0; i<3; i++) {
-                if(b[i][0] === b[i][1] && b[i][1] === b[i][2] && b[i][0] !=0) return b[i][0];
-                if(b[0][i] === b[1][i] && b[1][i] === b[2][i] && b[0][i] !=0) return b[0][i];
-            }
-
-            if(b[0][0] === b[1][1] && b[1][1] === b[2][2] && b[i][0] !=0) return b[i][0];
-
-            return 0;
-
-        }
-
         const winner = checkWinner();
 
         if(winner) {
@@ -107,7 +106,12 @@ function GameController(
         printNewRound();
         }
 
-        return result;
+        return {
+            playRound,
+            getActivePlayer,
+            getBoard,
+            checkWinner  
+        };
     }
 
     const getBoard = () => board.getBoard();
@@ -117,7 +121,8 @@ function GameController(
     return {
         playRound,
         getActivePlayer,
-        getBoard
+        getBoard: board.getBoard,
+        checkWinner
     };
 }
 
@@ -125,14 +130,21 @@ function ScreenController() {
     const game = GameController();
     const playerTurnDiv = document.querySelector(".turn");
     const boardDiv = document.querySelector(".board");
+    const winnerDiv = document.querySelector(".winner");
 
     const updateScreen = () => {
         boardDiv.textContent = "";
 
         const board = game.getBoard();
         const activePlayer = game.getActivePlayer();
+        const winner = game.checkWinner();
 
         playerTurnDiv.textContent = `${activePlayer.name}'s turn`
+        if(winner) {
+            winnerDiv.textContent = `${winner.name} wins!`;
+        } else {
+            winnerDiv.textContent = "";
+        }
 
         board.forEach((row, rowIndex) => {
             row.forEach((cell, columnIndex) => {
@@ -147,17 +159,12 @@ function ScreenController() {
     }
 
     function clickHandleButton(e) {
-        const selectedRow = parseInt(e.target.dataset.row);
-        const selectedColumn = parseInt(e.target.dataset.column);
-
+       
         if(e.target.classList.contains("cell")) {
-            const result = game.playRound(selectedRow, selectedColumn);
-            if (result.success) {
-                e.target.textContent = result.player.token === 1 ? "X" : "O";
-                updateScreen();
-            } else {
-               return;
-            }
+            const selectedRow = parseInt(e.target.dataset.row);
+            const selectedColumn = parseInt(e.target.dataset.column);    
+            game.playRound(selectedRow, selectedColumn);
+            updateScreen();
         }
     }
 
